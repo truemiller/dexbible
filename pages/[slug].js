da11aade00e6068413562f89f86d4150cc8cb01e
Josh <31908788+truemiller@users.noreply.github.com>
2022-10-16 02:51:27 +0100

    first commit

---
+++ pages/[slug].js
@@ -0,0 +1,14 @@
+import Home from "./index";
+import DefaultErrorPage from "next/error"
+import {useRouter} from "next/router";
+import NETWORKS from "../json/networks.json"
+
+export default function FilteredPage() {
+	const router = useRouter()
+	const { slug } = router.query
+	if (NETWORKS.find(network=>network.slug===slug)) {
+		return <Home initialFilter={slug}/>
+	} else {
+		return <h1>404 - Page Not Found</h1>
+	}
+}
